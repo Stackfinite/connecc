9f1ff77712800a09b2163d7d969876ee86fc474c
Indro Lie <Indrolie@gmail.com>
2018-07-29 16:12:48 +0700

    add code to store data to localstorage

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
 // General variabel
-let people = []
+let people = [];
 
 // Component variabel
-const formAdd = $('#form-add')
-const navHome = $('#nav-home')
-const navAbout = $('#nav-about')
-const navAddContactState = $('#nav-add-contact-state')
-const navShowContactState = $('#nav-show-contact-state')
-const cardContentWrapper = $('#card-content-wrapper')
-const dataPeopleField = $('#data-people-field')
+const formAdd = $("#form-add");
+const navHome = $("#nav-home");
+const navAbout = $("#nav-about");
+const navAddContactState = $("#nav-add-contact-state");
+const navShowContactState = $("#nav-show-contact-state");
+const cardContentWrapper = $("#card-content-wrapper");
+const dataPeopleField = $("#data-people-field");
 
 // Form field variabel
-const name = $('#name')
-const phoneNumber = $('#phone-number')
-const email = $('#email')
-const address = $('#address')
+let name = $("#name");
+let phoneNumber = $("#phone-number");
+let email = $("#email");
+let address = $("#address");
+const submit = $("#button-submit");
+const reset = $("#button-reset");
 
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // Function Definition
 const cardTemplate = (name, phoneNumber, email, address) => {
-    const card = `
+  const card = `
     <div class="card text-white bg-info contact-person" style="max-width: 18rem;">
         <div class="card-header">${name}</div>
         <div class="card-body">
@@ -29,56 +31,81 @@ const cardTemplate = (name, phoneNumber, email, address) => {
             <p class="card-text"><b>Address</b>: ${address}</p>
     </div>
     </div>
-    `
-    return dataPeopleField.append(card)
-}
+    `;
+  return dataPeopleField.append(card);
+};
 
-const get = () => {
-    const data = window.localStorage.getItem('People')
+const addLocalStorage = () => {
+  let newData = {
+    name: document.getElementById("name").value,
+    phoneNumber: document.getElementById("phone-number").value,
+    email: document.getElementById("email").value,
+    address: document.getElementById("address").value
+  };
 
-    if (data) return JSON.parse(data)
-    else return []
-}
+  people.push(newData);
+  window.localStorage.setItem("contacts", JSON.stringify(people));
+};
 
-const save = (data) => {
-    window.localStorage.setItem('People', JSON.stringify(data))
-}
+const getLocalStorage = () => {
+  const getData = window.localStorage.getItem("contacts");
+
+  if (getData) return (people = JSON.parse(getData));
+  else return [];
+};
+
+const clearAll = () => {
+  name[0].value = "";
+  phoneNumber[0].value = "";
+  email[0].value = "";
+  address[0].value = "";
+};
 
 const showData = () => {
-    // const dataPeople = get()
-    // dataPeopleField.html('')
+  // const dataPeople = get()
+  // dataPeopleField.html('')
 
-    // dataPeople.forEach(person => {
-    //     let personInfo = ''
-    //     for(let key in person){
+  // dataPeople.forEach(person => {
+  //     let personInfo = ''
+  //     for(let key in person){
 
-    //     }
-    // });
-    return cardTemplate("Arie Brainware","08123456","nobody@nowhere.net","Nowhere")
-}
+  //     }
+  // });
+  return cardTemplate(
+    "Arie Brainware",
+    "08123456",
+    "nobody@nowhere.net",
+    "Nowhere"
+  );
+};
 
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // Event listener
-navAbout.on('click', function () {
-    navHome.removeClass('active')
-    navAbout.addClass('active')
-})
+navAbout.on("click", function() {
+  navHome.removeClass("active");
+  navAbout.addClass("active");
+});
 
-navHome.on('click', function () {
-    navHome.addClass('active')
-    navAbout.removeClass('active')
-})
+navHome.on("click", function() {
+  navHome.addClass("active");
+  navAbout.removeClass("active");
+});
 
-navAddContactState.on('click', function () {
-    navAddContactState.addClass('active')
-    navShowContactState.removeClass('active')
-    cardContentWrapper.removeClass('display-hidden')
-})
+navAddContactState.on("click", function() {
+  navAddContactState.addClass("active");
+  navShowContactState.removeClass("active");
+  cardContentWrapper.removeClass("display-hidden");
+});
 
-navShowContactState.on('click', function () {
-    navAddContactState.removeClass('active')
-    navShowContactState.addClass('active')
-    cardContentWrapper.addClass('display-hidden')
-    dataPeopleField.removeClass('display-hidden')
-    showData()
-})
+navShowContactState.on("click", function() {
+  navAddContactState.removeClass("active");
+  navShowContactState.addClass("active");
+  cardContentWrapper.addClass("display-hidden");
+  dataPeopleField.removeClass("display-hidden");
+  showData();
+});
+
+submit.on("click", function() {
+  addLocalStorage();
+  clearAll();
+});
