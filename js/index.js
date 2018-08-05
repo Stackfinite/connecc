f6ebe8671b9e2c998c06d06dc952f2459f4072c6
indrolie <41278410+indrolie@users.noreply.github.com>
2018-08-05 19:40:04 +0700

    Revert "Change data storing method"

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Component/DOM variable
 const formAdd = $("#form-add");
 const navHome = $("#nav-home");
@@ -21,33 +23,43 @@ const searchKeyword = $("#search-keyword");
 // Function definition
 
 const successMessage = () => {
-  swal("Success!", "Contact successfully saved!", "success");
+	swal("Success!", "Contact successfully saved!", "success");
 };
 
-// Add data to JSON file
-const addContact = () => {
-  let newData = {
-    name: $("#name").val(),
-    phoneNumber: $("#phone-number").val(),
-    email: $("#email").val(),
-    address: $("#address").val()
-  };
+// Get data from local storage
+const getLocalStorage = () => {
+	const data = window.localStorage.getItem("contacts");
 
-  people.push(newData);
-  window.localStorage.setItem("contacts", JSON.stringify(people));
+	if (data) return JSON.parse(data);
+	else return [];
+};
+
+// Initial data
+let people = getLocalStorage();
+
+// Add data to localstorage
+const addLocalStorage = () => {
+	let newData = {
+		name: $("#name").val(),
+		phoneNumber: $("#phone-number").val(),
+		email: $("#email").val(),
+		address: $("#address").val()
+	};
+
+	people.push(newData);
+	window.localStorage.setItem("contacts", JSON.stringify(people));
 };
 
 // Clear all textbox
 const clearAll = () => {
-  name[0].value = "";
-  phoneNumber[0].value = "";
-  email[0].value = "";
-  address[0].value = "";
+	name[0].value = "";
+	phoneNumber[0].value = "";
+	email[0].value = "";
+	address[0].value = "";
 };
 
-// Template for contact display
 const createTemplate = (contact, index) => {
-  return `
+	return `
   <div class="col-12 col-md-4">
     <div id="contact-${index}" class="card text-white bg-info contact-person" style="max-width: 18rem;">
       <div class="card-header">
@@ -63,97 +75,88 @@ const createTemplate = (contact, index) => {
   `;
 };
 
-// Show data retrieved from JSON file
-const showContact = () => {
-  fetch("http://localhost:3000")
-    .then(response => response.json())
-    .then(data => {
-      dataPeopleField.html("");
+// Show data retrieved from localstorage
+const showData = () => {
+	const people = getLocalStorage();
+	dataPeopleField.html("");
 
-      data.contacts.forEach((contact, index) => {
-        const card = createTemplate(contact, index);
+	people.forEach((contact, index) => {
+		const card = createTemplate(contact, index);
 
-        dataPeopleField.append(card);
-      });
-    });
+		dataPeopleField.append(card);
+	});
 };
 
-// Search contact stored in JSON file
 const searchContact = () => {
-  const keyword = $("#search-keyword").val();
-  const keywordLowercase = keyword.toLowerCase();
-  const people = getLocalStorage();
-  dataPeopleField.html("");
+	const keyword = $("#search-keyword").val();
+	const keywordLowercase = keyword.toLowerCase();
+	const people = getLocalStorage();
+	dataPeopleField.html("");
 
-  people.forEach((contact, index) => {
-    const found =
-      contact.name.toLowerCase().indexOf(keywordLowercase) !== -1 ||
-      contact.phoneNumber.toLowerCase().indexOf(keywordLowercase) !== -1 ||
-      contact.email.toLowerCase().indexOf(keywordLowercase) !== -1 ||
-      contact.address.toLowerCase().indexOf(keywordLowercase) !== -1;
+	people.forEach((contact, index) => {
+		const found =
+			contact.name.toLowerCase().indexOf(keywordLowercase) !== -1 ||
+			contact.phoneNumber.toLowerCase().indexOf(keywordLowercase) !== -1 ||
+			contact.email.toLowerCase().indexOf(keywordLowercase) !== -1 ||
+			contact.address.toLowerCase().indexOf(keywordLowercase) !== -1;
 
-    if (found) {
-      const card = createTemplate(contact, index);
-      dataPeopleField.append(card);
-    }
-  });
+		if (found) {
+			const card = createTemplate(contact, index);
+			dataPeopleField.append(card);
+		}
+	});
 };
 
 // Validate if field empty
 const validate = () => {
-  if (
-    name.val() === "" ||
-    phoneNumber.val() === "" ||
-    address.val() === "" ||
-    email.val() === ""
-  ) {
-    swal("Error!", "Please fill every field!", "error");
-  } else {
-    addContact();
-    successMessage();
-    clearAll();
-  }
+	if (name.val() === "" || phoneNumber.val() === "" || address.val() === "" || email.val() === "") {
+		swal("Error!", "Please fill every field!", "error");
+	} else {
+		addLocalStorage();
+		successMessage();
+		clearAll();
+	}
 };
 
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // Event listener
 window.addEventListener("load", function() {
-  navAbout.on("click", function() {
-    navHome.removeClass("active");
-    navAbout.addClass("active");
-  });
+	navAbout.on("click", function() {
+		navHome.removeClass("active");
+		navAbout.addClass("active");
+	});
 
-  navHome.on("click", function() {
-    navHome.addClass("active");
-    navAbout.removeClass("active");
-  });
+	navHome.on("click", function() {
+		navHome.addClass("active");
+		navAbout.removeClass("active");
+	});
 
-  navAddContactState.on("click", function() {
-    navAddContactState.addClass("active");
-    navShowContactState.removeClass("active");
-    cardContentWrapper.removeClass("display-hidden");
-    dataPeopleField.addClass("display-hidden");
-  });
+	navAddContactState.on("click", function() {
+		navAddContactState.addClass("active");
+		navShowContactState.removeClass("active");
+		cardContentWrapper.removeClass("display-hidden");
+		dataPeopleField.addClass("display-hidden");
+	});
 
-  navShowContactState.on("click", function() {
-    navAddContactState.removeClass("active");
-    navShowContactState.addClass("active");
-    cardContentWrapper.addClass("display-hidden");
-    dataPeopleField.removeClass("display-hidden");
-    showContact();
-  });
+	navShowContactState.on("click", function() {
+		navAddContactState.removeClass("active");
+		navShowContactState.addClass("active");
+		cardContentWrapper.addClass("display-hidden");
+		dataPeopleField.removeClass("display-hidden");
+		showData();
+	});
 
-  formAdd.on("submit", function() {
-    event.preventDefault();
-    validate();
-  });
+	formAdd.on("submit", function() {
+		event.preventDefault();
+		validate();
+	});
 
-  searchForm.on("submit", function(event) {
-    event.preventDefault();
-    searchContact();
-  });
+	searchForm.on("submit", function(event) {
+		event.preventDefault();
+		searchContact();
+	});
 
-  searchKeyword.on("input", function() {
-    searchContact();
-  });
+	searchKeyword.on("input", function() {
+		searchContact();
+	});
 });
